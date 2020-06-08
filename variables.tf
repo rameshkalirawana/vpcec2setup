10a30f23888501419e01a202d77659551bdd1622
ramesh <linux@LSIN819447.asiapacific.bmw.corp>
2020-06-08 11:02:04 +0530

    terraform demo code

---
+++ variables.tf
@@ -0,0 +1,38 @@
+# Variables
+variable "access_key" {
+	default = "AKIA2CXJOBLPKNCDSG6I"
+}
+variable "secret_key" {
+	default = "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"
+}
+variable "region" {
+  default = "us-east-2"
+}
+variable "cidr_vpc" {
+  description = "CIDR block for the VPC"
+  default = "10.1.0.0/16"
+}
+variable "cidr_subnet" {
+  description = "CIDR block for the subnet"
+  default = "10.1.0.0/24"
+}
+variable "availability_zone" {
+  description = "availability zone to create subnet"
+  default = "us-east-2a"
+}
+variable "public_key_path" {
+  description = "Public key path"
+  default = "~/.ssh/id_rsa.pub"
+}
+variable "instance_ami" {
+  description = "AMI for aws EC2 instance"
+  default = "ami-07c1207a9d40bc3bd"
+}
+variable "instance_type" {
+  description = "type for aws EC2 instance"
+  default = "t2.micro"
+}
+variable "environment_tag" {
+  description = "Environment tag"
+  default = "Ramesh-terraform"
+}
